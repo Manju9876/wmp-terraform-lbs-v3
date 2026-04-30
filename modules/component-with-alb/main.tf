689baf91427845ea8334e90e8afdaa8e2c812c59
Manju9876 <manjunathb960650@gmail.com>
2026-04-30 16:30:50 +0530

    github actions practice

--- modules/component-with-alb/main.tf
+++ modules/component-with-alb/main.tf
@@ -1,6 +1,28 @@
-resource "aws_security_group" "main" {
+resource "aws_security_group" "instance" {
 
-  name = "${var.component_name}-${var.env}"
+  name = "${var.component_name}-${var.env}-instance"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+     }
+
+  tags = {
+    Name = "${var.component_name}-${var.env}-instance"
+  }
+}
+
+resource "aws_security_group" "alb" {
+
+  name = "${var.component_name}-${var.env}-alb"
 
   dynamic "ingress" {
     for_each = var.ports
@@ -18,24 +40,47 @@ resource "aws_security_group" "main" {
     to_port   = 0
     protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-     }
+  }
 
   tags = {
-    Name = "${var.component_name}-${var.env}"
+    Name = "${var.component_name}-${var.env}-alb"
   }
 }
-
 resource "aws_launch_template" "main" {
   name = "${var.component_name}-${var.env}"
   image_id = data.aws_ami.ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.main.id]
+  vpc_security_group_ids = [aws_security_group.instance.id,aws_security_group.alb.id]
 
   tag_specifications {
     resource_type = "instance"
     tags = {
       Name = "${var.component_name}-${var.env}"
     }
+  }
+}
+
+resource "aws_autoscaling_group" "main" {
+  availability_zones = ["us-east-1a","us-east-1b"]
+  desired_capacity   = var.asg_min_size
+  max_size           = var.asg_max_size
+  min_size           = var.asg_min_size
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_lb" "main" {
+  name               = "${var.component_name}-${var.env}"
+  internal           = var.alb_internal
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = var.alb_subnets
+
+  tags = {
+    Environment = "${var.component_name}-${env}"
   }
 }
 
@@ -54,32 +99,32 @@ resource "aws_launch_template" "main" {
 resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.main.zone_id
   name    = "${var.component_name}-${var.env}"
-  type    = "A"
+  type    = "CNAME"
   ttl     = 30
-  records = [aws_instance.main.private_ip]
+  records = [aws_lb.main.dns_name]
 }
 
-resource "null_resource" "ansible_code" {
-# depends_on = [aws_route53_record.main]
-
-  triggers = {
-    instance_id_change = aws_instance.main.id
-    #always_run = timestamp()
-  }
-
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      #      private_key = file(var.private_key_pem)
-      password = "DevOps321"
-      host     = aws_instance.main.public_ip
-    }
-
-    inline = [
-      "sudo python3.11 -m pip install ansible hvac",
-      "ansible-pull -i localhost, -U https://github.com/Manju9876/wmp-ansible wmp.yaml -e component_name=${var.component_name} -e env=${var.env}"
-    ]
-  }
-}
+# resource "null_resource" "ansible_code" {
+# # depends_on = [aws_route53_record.main]
+#
+#   triggers = {
+#     instance_id_change = aws_instance.main.id
+#     #always_run = timestamp()
+#   }
+#
+#   provisioner "remote-exec" {
+#
+#     connection {
+#       type     = "ssh"
+#       user     = "ec2-user"
+#       #      private_key = file(var.private_key_pem)
+#       password = "DevOps321"
+#       host     = aws_instance.main.public_ip
+#     }
+#
+#     inline = [
+#       "sudo python3.11 -m pip install ansible hvac",
+#       "ansible-pull -i localhost, -U https://github.com/Manju9876/wmp-ansible wmp.yaml -e component_name=${var.component_name} -e env=${var.env}"
+#     ]
+#   }
+# }
--- modules/component-with-alb/variables.tf
+++ modules/component-with-alb/variables.tf
@@ -3,3 +3,8 @@ variable "env" {}
 variable "instance_type" {}
 variable "ports" {}
 variable "dns_domain" {}
+
+variable "asg_min_size" {}
+variable "asg_max_size" {}
+variable "alb_internal" {}
+variable "alb_subnets" {}
