1346adeb8c5b25df50461f45f30d16dff3717313
Manju9876 <manjunathb960650@gmail.com>
2026-05-04 21:39:01 +0530

    github actions  Prctice

--- modules/component-with-alb/main.tf
+++ modules/component-with-alb/main.tf
@@ -51,11 +51,12 @@ resource "aws_launch_template" "main" {
   image_id = data.aws_ami.ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.instance.id,aws_security_group.alb.id]
-  user_data = base64encode(("${path.module}/user_data.sh"),
+  user_data = base64encode(
+    templatefile("${path.module}/user_data.sh",
     {
       ENV = var.env
       COMPONENT = var.component_name
-    }
+    })
   )
 
   tag_specifications {
