46af0317afe56eb2480cd161af02fcc597d57b2e
Manju9876 <manjunathb960650@gmail.com>
2026-05-04 22:19:01 +0530

    github actions  Prctice

--- modules/component-with-alb/main.tf
+++ modules/component-with-alb/main.tf
@@ -124,7 +124,16 @@ resource "aws_lb" "main" {
 #
 #   }
 #   }
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = var.alb["ports"]
+  protocol          = "HTTP"
 
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
 
 resource "aws_route53_record" "main" {
   zone_id = data.aws_route53_zone.main.zone_id
