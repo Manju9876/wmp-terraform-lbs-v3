50a3cccb4854c1b62d9736ab96afc29a582a00e5
Manju9876 <manjunathb960650@gmail.com>
2026-05-05 07:31:53 +0530

    github actions  Prctice

--- modules/component-with-alb/user_data.sh
+++ modules/component-with-alb/user_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo python3.11 -m pip install ansible hvac
+#sudo python3.11 -m pip install ansible hvac
+sudo labauto ansible
 ansible-pull -i localhost, -U https://github.com/Manju9876/wmp-ansible wmp.yaml -e component_name=${COMPONENT} -e env=${ENV}
