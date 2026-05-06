08997368a0475ee4de2113e834516b641c348543
Manju9876 <manjunathb960650@gmail.com>
2026-05-06 14:34:01 +0530

    github actions practice

--- modules/component-with-alb/user_data.sh
+++ modules/component-with-alb/user_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo labauto ansible
-ansible-pull -i localhost, -U https://github.com/Manju9876/wmp-ansible wmp.yml -e env=${ENV} -e component_name=${COMPONENT}
+ansible-pull -i localhost, -U https://github.com/Manju9876/wmp-ansible wmp.yaml -e env=${ENV} -e component_name=${COMPONENT}
