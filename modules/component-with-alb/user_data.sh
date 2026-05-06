4f20950ac9636cdc9a9b8403986c9d763012e137
Manju9876 <manjunathb960650@gmail.com>
2026-05-06 15:05:51 +0530

    github actions practice

--- modules/component-with-alb/user_data.sh
+++ modules/component-with-alb/user_data.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -euxo pipefail
 
-sudo labauto ansible
-ansible-pull -i localhost, -U https://github.com/Manju9876/wmp-ansible wmp.yaml -e env=${ENV} -e component_name=${COMPONENT}
+sudo dnf install -y python3-pip
+sudo pip3 install ansible hvac
+#sudo python3.11 -m pip install ansible hvac
+ansible-pull -i localhost, \
+  -U https://github.com/Manju9876/wmp-ansible \
+  wmp.yaml \
+  -e env=${ENV} \
+  -e component_name=${COMPONENT}
